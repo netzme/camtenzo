4fa200396d625af10f043ba94a448af5f3f479b3
daori <daori@quantumfin.com>
2014-10-16 08:43:30 +0700

    Testing Activity dan view activity_filter_image

---
+++ app/src/main/java/com/katenzo/camtenzo/FilterImage.java
@@ -0,0 +1,12 @@
+package com.katenzo.camtenzo;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by daori on 10/8/14.
+ */
+public class FilterImage {
+    public static Bitmap convertToGrayScale(Bitmap src){
+        return null;
+    }
+}
--- app/src/main/java/com/katenzo/camtenzo/FilterImageActivity.java
+++ app/src/main/java/com/katenzo/camtenzo/FilterImageActivity.java
@@ -6,21 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class CameraActivity extends Activity {
-
-    public static final String EXTRA_MESSAGE = "Message New Image";
+public class FilterImageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.layout.activity_filter_image);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.camera, menu);
+        getMenuInflater().inflate(R.menu.filter_image, menu);
         return true;
     }
 
